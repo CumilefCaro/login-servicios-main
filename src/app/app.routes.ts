d07c34510910e078612279a82f3eb1ea75d4811c
CaroCumilef <ca.cumilef@duocuc.cl>
2024-09-30 00:14:39 -0300

    Inicial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  }
+];
